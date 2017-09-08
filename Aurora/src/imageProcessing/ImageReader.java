f1072f834d8951fe11110a0f0d503b18e07c3953
Bret Reynolds <sbreyno3@ncsu.edu>
2017-09-08 00:00:22 -0400

    initial commit of Aurora with

---
+++ Aurora/src/imageProcessing/ImageReader.java
@@ -0,0 +1,81 @@
+package imageProcessing;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+/**
+ * 
+ * @author Bret
+ *
+ */
+public class ImageReader {
+    public static void main(String [] args) {
+    	
+    	String fileName = "samples/MARBLES";
+    	
+    	try 
+    	{
+    		//create file streams
+    		FileInputStream fis = new FileInputStream(fileName + ".bmp");
+    		FileOutputStream fos = new FileOutputStream(fileName + ".txt");
+    		Writer w = new OutputStreamWriter(fos, "UTF8");
+    		
+    		int i;
+    		int count = 1;
+    		//read each byte to an integer
+    		while((i = fis.read()) != -1){
+    			//System.out.println(count + " " + i);
+    			
+    			//w.append(count++ + ": " + i + " -> " + intToHex(i) + "; ");
+    			w.append(i + " -> " + intToHex(i) + ";      ");
+    			count++;
+    			if(count % 8 == 0)
+    				w.append("\r\n");
+    		}
+			w.close();
+			fos.close();
+			fis.close();
+		} 
+    	catch (IOException e) 
+    	{
+			System.out.println("broken");
+		}
+    }
+    
+    /**
+     * turs an integer into hexidecimal
+     * @param i
+     * @return
+     */
+    private static String intToHex(int i){
+    	String rtn = "";
+    	
+    	int next = i / 16;
+    	
+    	if(next > 0)
+    		rtn += intToHex(next);
+    	
+    	i %= 16;
+    	if(i < 10)
+    		return rtn += i;
+    	switch (i) 
+    	{
+    	case 10:
+    		return rtn += "A";
+    	case 11:
+    		return rtn += "B";
+    	case 12:
+    		return rtn += "C";
+    	case 13:
+    		return rtn += "D";
+    	case 14:
+    		return rtn += "E";
+    	case 15:
+    		return rtn += "F";
+    	}
+    	return rtn;
+    }
+       
+}
