a474dcff4ddf5ed0fc418ec046d77603d430ae6e
Bret Reynolds <sbreyno3@ncsu.edu>
2017-09-08 03:00:26 -0400

    added gray scale function from bmp files

--- Aurora/src/imageProcessing/ImageReader.java
+++ Aurora/src/imageProcessing/ImageReader.java
@@ -1,10 +1,12 @@
 package imageProcessing;
 
+import java.awt.image.*;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.ArrayList;
 /**
  * 
  * @author Bret Reynolds and Jake Hackim
@@ -13,29 +15,39 @@ import java.io.Writer;
 public class ImageReader {
     public static void main(String [] args) {
     	
-    	String fileName = "samples/MARBLES";
+    	String fileName = "samples/test";
     	
     	try 
     	{
     		//create file streams
     		FileInputStream fis = new FileInputStream(fileName + ".bmp");
-    		FileOutputStream fos = new FileOutputStream(fileName + ".txt");
-    		Writer w = new OutputStreamWriter(fos, "UTF8");
+    		//use for file writing
+    		//FileOutputStream fos = new FileOutputStream(fileName + ".txt");
+    		//Writer w = new OutputStreamWriter(fos, "UTF8");
     		
     		int i;
-    		int count = 1;
+    		int count = 0;
+    		ArrayList<Integer> bitmap = new ArrayList<Integer>();
     		//read each byte to an integer
-    		while((i = fis.read()) != -1){
-    			//System.out.println(count + " " + i);
-    			
-    			//w.append(count++ + ": " + i + " -> " + intToHex(i) + "; ");
+    		
+    		while((i = fis.read()) != -1)
+    		{
+    			bitmap.add(i);
+    			count++;
+    			 //use if a txt file needs to be created
+    			/*
     			w.append(i + " -> " + intToHex(i) + ";      ");
     			count++;
-    			if(count % 8 == 0)
+    			if((count + 2) % 4 == 0)
     				w.append("\r\n");
+    			*/			
     		}
-			w.close();
-			fos.close();
+    		int idx = bitmap.get(10);
+    		int[] graymap = grayscale(bitmap, idx);
+    		
+    		//use for file writing
+			//w.close();
+			//fos.close();
 			fis.close();
 		} 
     	catch (IOException e) 
@@ -45,11 +57,12 @@ public class ImageReader {
     }
     
     /**
-     * turs an integer into hexidecimal
-     * @param i
+     * turns an integer into hexadecimal
+     * @param i 
      * @return
      */
-    private static String intToHex(int i){
+    private static String intToHex(int i)
+    {
     	String rtn = "";
     	//recursive call if current value is greater than 16
     	if(i >= 16)
@@ -76,4 +89,20 @@ public class ImageReader {
     	return rtn;
     }
        
+    private static int[] grayscale(ArrayList<Integer> bitmap, int idx)
+    {
+    	int graysize = (bitmap.size() - idx - 1) / 4;
+    	
+    	int[] graymap = new int[graysize];
+    	
+    	for( int i = 0; i < graysize; ++i)
+    	{
+    		graymap[i] = (int)(0.07 * bitmap.get(idx + i * 3) + 0.72 * bitmap.get(idx + i * 3 + 1) + 0.21 * bitmap.get(idx + i * 3 + 2));
+    		
+    		System.out.println(i + ": " + bitmap.get(idx + i * 3 + 2) + " " + bitmap.get(idx + i * 3 + 1) + " " + bitmap.get(idx + i * 3) + ": " + graymap[i]);
+    		++idx;
+    	}
+    			
+    	return graymap;
+    }
 }
